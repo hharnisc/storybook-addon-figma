8ae5c52a623da69dbcff0522120be6e888b4b3d2
Harrison Harnisch <hharnisc@gmail.com>
2017-10-10 15:13:46 -0500

    use a more realistic example

--- .storybook/Button.story.jsx
+++ .storybook/Button.story.jsx
@@ -2,22 +2,44 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { WithFigma } from '../index'
 
+const style = {
+      margin: 0,
+      padding: '0.7rem 1.5rem',
+      background: '#168eea',
+      color: 'white',
+      border: 0,
+      borderRadius: '2rem',
+      cursor: 'pointer',
+    };
+
 storiesOf('Button')
   .add('With Figma', () => (
     <WithFigma
-      url={'https://www.figma.com/file/LKQ4FJ4bTnCSjedbRpk931/Sample-File'}
+      url={'https://www.figma.com/file/LbcvMJxDtshDmYtdyfJfkA72/Button-Primary'}
     >
-      <button>Hi 👋</button>
+      <button
+        style={style}
+      >
+        Button Primary
+      </button>
     </WithFigma>
   ))
   .add('With Figma No Fullscreen Option', () => (
     <WithFigma
-      url={'https://www.figma.com/file/LKQ4FJ4bTnCSjedbRpk931/Sample-File'}
+      url={'https://www.figma.com/file/LbcvMJxDtshDmYtdyfJfkA72/Button-Primary'}
       allowFullScreen={false}
     >
-      <button>Hi 👋</button>
+      <button
+        style={style}
+      >
+        Button Primary
+      </button>
     </WithFigma>
   ))
   .add('Without Figma', () => (
-    <button>Hi 👋</button>
+    <button
+      style={style}
+    >
+      Button Primary
+    </button>
   ))
