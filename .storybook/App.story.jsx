779cdcc6b2b7b42621b7912caaa2af80bef1aa8b
Harrison Harnisch <hharnisc@gmail.com>
2017-10-10 15:26:28 -0500

    add application example

--- .storybook/App.story.jsx
+++ .storybook/App.story.jsx
@@ -7,9 +7,39 @@ storiesOf('App')
     url: 'https://www.figma.com/file/LKQ4FJ4bTnCSjedbRpk931/Sample-File',
     allowFullScreen: true
   }))
-  .add('default', () => (
-    <button>Hi 👋</button>
-  ))
-  .add('another default', () => (
-    <button>Hello! 👋</button>
+  .add('My App', () => (
+    <div
+      style={{
+        background: 'black',
+        color: 'white',
+        height: 640,
+        width: 320,
+        fontFamily: 'Arial',
+      }}
+    >
+      <div
+        style={{
+          padding: '1rem 0',
+          textAlign: 'center'
+        }}
+      >
+        @Khloe
+      </div>
+      <div
+        style={{
+          padding: '1rem 0',
+          textAlign: 'center'
+        }}
+      >
+        <div
+          style={{
+            background: 'white',
+            height: '5rem',
+            width: '5rem',
+            borderRadius: '10rem',
+            display: 'inline-block'
+          }}
+        />
+      </div>
+    </div>
   ))
